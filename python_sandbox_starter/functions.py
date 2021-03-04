204b24ef3fb57d3d23a336eaa98565b9fb5bcfd4
Lucas Teixeira <lucasteixeira@3457-NB.local>
2021-03-04 17:55:56 -0300

    Correct things

--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -5,3 +5,4 @@
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
+print('Hello, Lucas!')
