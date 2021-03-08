1724c73a5a2c6e2054e1bc0ee2e6febbd8963f22
Lucas Teixeira <lucasteixeira@3457-NB.local>
2021-03-07 22:58:53 -0300

    Listings arrumado

--- btre_project/btre/urls.py
+++ btre_project/btre/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
-    path('listing/', include('listings.urls'))
+    path('listings/', include('listings.urls')),
+    path('admin/', admin.site.urls)
 ]
--- btre_project/listings/views.py
+++ btre_project/listings/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    render(request, 'listings/listings.html')
+    return render(request, 'listings/listings.html')
 
 def listing(request):
-    render(request, 'listings/listings.html')
+    return render(request, 'listings/listings.html')
 
 def search(request):
-    render(request, 'listings/search.html')
+    return render(request, 'listings/search.html')
     
