46ace96f3695c20912d12a4fa9d537c23cce2684
Lucas Teixeira <lucas.teixeira@ifood.com.br>
2021-03-24 13:36:23 -0300

    Logging into dashboard OK

--- btre_project/accounts/views.py
+++ btre_project/accounts/views.py
@@ -41,7 +41,18 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # Login user.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials.')
+            return redirect('login')
         return
     else:
         return render(request, 'accounts/login.html')
